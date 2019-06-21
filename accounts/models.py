00476d0d4a1b4d37065f3fb3687adc53f5373f8a
cmigazzi <cedric.migazzi@hotmail.fr>
2019-06-21 10:01:01 +0200

    Add flags for multiple type users.

--- accounts/models.py
+++ accounts/models.py
@@ -9,7 +9,24 @@ class User(AbstractBaseUser):
 
     email = models.EmailField(verbose_name="email address",
                               unique=True)
+    is_admin = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_teacher = models.BooleanField(default=False)
+    is_school = models.BooleanField(default=False)
 
     USERNAME_FIELD = "email"
 
     objects = UserManager()
+
+    def has_perm(self, perm, obj=None):
+        """Check if the user have a specific permission."""
+        return True
+
+    def has_module_perms(self, app_label):
+        """Check if the user have permissions to view the app 'app_label'."""
+        return True
+
+    @property
+    def is_staff(self):
+        """Check if the user is a member of staff."""
+        return self.is_admin
--- accounts/tests/unit_tests/test_models.py
+++ accounts/tests/unit_tests/test_models.py
@@ -19,6 +19,14 @@ def test_username():
 
 def test_user_attributes():
     assert hasattr(User, "email")
+    assert hasattr(User, "is_active")
+    assert hasattr(User, "is_admin")
+    assert hasattr(User, "is_teacher")
+    assert hasattr(User, "is_school")
+
+    assert hasattr(User, "has_perm")
+    assert hasattr(User, "has_module_perms")
+    assert hasattr(User, "is_staff")
 
 
 def test_user_creation(django_user_model):
@@ -33,3 +41,21 @@ def test_string_representation():
 
 def test_user_manager():
     assert isinstance(User.objects, UserManager)
+
+
+def test_has_perm():
+    perm = "home.can_i"
+    assert User().has_perm(perm) is True
+
+
+def test_has_module_perms():
+    app_label = "home"
+    assert User().has_module_perms(app_label) is True
+
+
+def test_is_staff_is_property():
+    assert isinstance(type(User()).is_staff, property)
+
+
+def test_is_staff():
+    assert User().is_staff == User().is_admin
