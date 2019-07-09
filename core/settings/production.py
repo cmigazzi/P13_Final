0d95ecab23f63f3bc0de2f78032676aaef542ab4
cmigazzi <cedric.migazzi@hotmail.fr>
2019-07-09 22:37:49 +0200

    change email settings.

--- core/settings/production.py
+++ core/settings/production.py
@@ -1,3 +1,11 @@
 from . import *
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+EMAIL_HOST = os.environ.get("EMAIL_HOST")
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
+EMAIL_PORT = 465
+EMAIL_USE_SSL = True
