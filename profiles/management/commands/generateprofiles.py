7bc1e4360da5cda4ce16990a5ea62f909d549d46
cmigazzi <cedric.migazzi@hotmail.fr>
2019-07-11 19:12:34 +0200

    add teacher profile fixtures.

--- profiles/management/commands/generateprofiles.py
+++ profiles/management/commands/generateprofiles.py
@@ -4,8 +4,9 @@ from django.core.management.base import BaseCommand
 from django.core.management import call_command
 from django.contrib.auth import get_user_model
 
-from .profiles import SCHOOL_TYPES
-from profiles.models import School
+from .profiles import (SCHOOL_TYPES, PHONES, SCHOOL_NAMES,
+                       FIRST_NAMES, LAST_NAMES)
+from profiles.models import School, Teacher
 
 User = get_user_model()
 
@@ -17,10 +18,21 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         """Handle command."""
-        users = User.objects.filter(is_school=True)
+        users = User.objects.all()
         for user in users:
-            profile = School(user=user,
-                             school_type=random.choice(SCHOOL_TYPES))
+            if user.is_school:
+                profile = School(user=user,
+                                 name=random.choice(SCHOOL_NAMES),
+                                 phone=random.choice(PHONES),
+                                 school_type=random.choice(SCHOOL_TYPES))
+            elif user.is_teacher:
+                profile = Teacher(user=user,
+                                  first_name=random.choice(FIRST_NAMES),
+                                  last_name=random.choice(LAST_NAMES),
+                                  phone=random.choice(PHONES),
+                                  )
+            else:
+                continue
             profile.save()
         call_command("dumpdata",
                      "profiles.School",
--- profiles/management/commands/profiles.py
+++ profiles/management/commands/profiles.py
@@ -1,1 +1,19 @@
 SCHOOL_TYPES = ["Conservatoire", "Ecole de musique", "Association"]
+
+SCHOOL_NAMES = ["Ecole de musique VDI",
+                "Conservatoire de Lyon",
+                "Conservatoire de Metz",
+                "Ecole de musique de St Didier",
+                "Conservatoire de Villeurbanne",
+                "Ecole intercommunlae du Lyonnais",
+                "Conservatoire de Macon",
+                "Conservatoire Grenoble"]
+
+PHONES = ["0450236598",
+          "0236548965",
+          "0154879653",
+          "0653265987"]
+
+FIRST_NAMES = ["Jean", "Michel", "Fabienne", "Martine"]
+
+LAST_NAMES = ["Durand", "Dupont", "Martin", "Carnot"]
